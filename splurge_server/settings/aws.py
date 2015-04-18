7e015c01bf95e72467f0436dd9d958dfd3043db0
Nagkumar <me@nagkumar.com>
2015-04-18 16:32:07 -0700

    Modified settings

--- splurge_server/settings/aws.py
+++ splurge_server/settings/aws.py
@@ -1,5 +1,21 @@
 __author__ = 'nagkumar'
 from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['getsplurge.com', 'splurge.nagkumar.com']
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = "/var/www/splurge/media"
+
+MEDIA_URL = '/media/'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
