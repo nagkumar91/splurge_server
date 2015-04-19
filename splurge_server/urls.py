0b913751e2a1270417ad51071c516c23ed8090ab
Nagkumar <me@nagkumar.com>
2015-04-18 20:04:34 -0700

    Added url

--- splurge_server/urls.py
+++ splurge_server/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 admin.autodiscover()
 urlpatterns = [
     # Examples:
@@ -7,4 +9,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
 ]
